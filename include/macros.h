1d00cc2c4c0c9525c4bed985589478178e1efdde
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-08-03 13:35:39 -0400

    Add files via upload

---
+++ include/macros.h
@@ -0,0 +1,18 @@
+
+/*
+ * macros.h
+ * NOTE: avoid magic numbers; use macros
+*/
+
+
+// flywheel speed (in rpm)
+// 600 is the max rpm of the vex motors
+#define FLYWHEEL_RPM 600
+
+// drivetrain speed (in rpm)
+// have our robot go 100rpm so its not too sensitive
+#define DRIVETRAIN_SPEED 100 
+
+// wait time (in seconds)
+// amount of time to wait for automation
+#define WAIT_TIME 3 
